89734e88e85d3c0a5e7a6b6f04e27e207e5738c2
hanzla-6khan <hanzalakhan101010@gmail.com>
2023-12-24 00:33:57 +0500

    frontent

--- src/app/Modules/mainModule/components/contact/contact.component.ts
+++ src/app/Modules/mainModule/components/contact/contact.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +11,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
+  faLock = faLock;
+  myForm: FormGroup | any;
 
+  constructor(private fb: FormBuilder, private auth: AuthService, private router: Router, private toastr: ToastrService) { }
+
+  ngOnInit() {
+    this.myForm = this.fb.group({
+      emailFormControl: ['', [Validators.required, Validators.email]],
+      passwordFormControl: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  get emailFormControl() {
+    return this.myForm.get('emailFormControl');
+  }
+
+  get passwordFormControl() {
+    return this.myForm.get('passwordFormControl');
+  }
+
+  onSubmit() {
+    if (this.myForm.valid) {
+      this.auth.login(this.myForm.value).subscribe(
+        (result) => {
+          this.toastr.success('login successfully !', 'Success');
+
+          this.router.navigate(['./admin']);
+          this.myForm.close()
+        },
+        (err: Error) => {
+          this.toastr.error('invalid credentials !', 'failed');
+        }
+      );
+    } else {
+
+      this.myForm.markAllAsTouched();
+    }
+  }
 }
